29ab452729a1c8a6f3f02fba80828116a2e82996
Christopher Jones <chrisjns679@gmail.com>
2020-06-09 15:29:22 -0400

    Added gradient text flow, full text.

--- scripts/script.js
+++ scripts/script.js
@@ -36,39 +36,28 @@ function collision_calc(ballOne, ballTwo, indexOne, indexTwo) {
         firstCircles[indexTwo].color = getRndHexColor();
     }
 
-    //handling overlap
-    // if(deltaX < radius * 2 && deltaY < radius * 2) {
-    //     if (ballOne.x > ballTwo.x) {
-    //
-    //         firstCircles[indexOne].x += (radius - (ballOne.x - radius - ballTwo.x)) / 2;
-    //         firstCircles[indexTwo].x -= (radius - (ballOne.x - radius - ballTwo.x)) / 2;
-    //     } else {
-    //         firstCircles[indexTwo].x += (radius - (ballTwo.x - radius - ballOne.x)) / 2;
-    //         firstCircles[indexOne].x -= (radius - (ballTwo.x - radius - ballOne.x)) / 2;
-    //     }
-    //
-    //
-    //     if (ballOne.y > ballTwo.y) {
-    //         firstCircles[indexOne].y += radius - (ballOne.y - radius - ballTwo.y);
-    //     } else {
-    //         firstCircles[indexTwo].y += radius - (ballTwo.y - radius - ballOne.y);
-    //     }
-    // }
-
 }
+
+const themeColors = ["#00ffff", "#9952e0", "#ff7bac", "#3fa9f5", "#FFF"];
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 
-const pageWidth = window.innerWidth;
-const pageHeight =  window.innerHeight;
+let pageWidth = window.innerWidth;
+let pageHeight =  window.innerHeight;
+
+let vmin = pageHeight < pageWidth ? pageHeight / 100 : pageWidth / 100;
+let vmax = pageHeight >= pageWidth ? pageWidth / 100 : pageHeight / 100;
+
 
 ctx.canvas.width  = pageWidth;
 ctx.canvas.height = pageHeight;
 
 ctx.fillStyle = 'white';
 ctx.strokeStyle = 'white';
+
+// ctx.font = ctx.font = "2em League Spartan";
 
 let randomWidth = getRndInteger(0, (pageWidth));
 let randomHeight = getRndInteger(0, (pageHeight));
@@ -77,6 +66,7 @@ let randomx = getRndInteger(0, (pageWidth));
 let randomy = getRndInteger(0, (pageHeight));
 
 
+let animationFrame;
 
 let radius = 25;
 
@@ -84,7 +74,7 @@ let gap = 5;
 
 //ctx.fillRect((pageWidth / 2) - (randomWidth / 2), (pageHeight / 2) - (randomHeight / 2), randomWidth, randomHeight);
 
-const firstCircles = [];
+let firstCircles = [];
 
 // ctx.beginPath();
 // ctx.fillStyle = 'white';
@@ -133,15 +123,88 @@ function firstDraw() {
         }
 
     }
-    window.requestAnimationFrame(update);
+    animationFrame = window.requestAnimationFrame(update);
 }
 
-let tick = 0;
+let tick = true;
 let nextframe;
 let collide_buffer = new Set();
+let i = 0;
+let j = 0;
+let k = 0;
+
+let color2 = 'white';
+let color1 = 'cyan';
+
 function update() {
+    // if(j > pageWidth && i > pageHeight) {
+    //     j = 0;
+    //     i = 0;
+    //     // let col = color2;
+    //     // color2 = color1;
+    //     // color1 = col;
+    // }
+
+    if(j > 100) {
+        tick = false;
+        j = 100;
+    } else if(j < 0) {
+        tick = true;
+        j = 0;
+    }
+
+    let gradient = ctx.createLinearGradient(0,0, pageWidth, pageHeight);
+    gradient.addColorStop(0, color1);
+
+    gradient.addColorStop(j/100, color2);
+
+    gradient.addColorStop(1, color1);
+
+
 
     ctx.clearRect(0, 0, pageWidth, pageHeight);
+    ctx.font = "6vmin League Spartan";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+
+    let text1 = "Christopher Jones";
+    let text = "Full Stack Developer"
+    let text2 = "Bringing Order to the Chaos"
+    let textLeft = Math.floor((pageWidth/2) - (ctx.measureText(text).width / 2) - radius);
+    let textRight = Math.ceil((ctx.measureText(text).actualBoundingBoxRight + (pageWidth/2) + radius));
+    let textTop = Math.floor((pageHeight/2) - ctx.measureText(text).actualBoundingBoxAscent - radius);
+    let textBottom = Math.ceil(ctx.measureText(text).actualBoundingBoxDescent) + (pageHeight/2) + radius;
+    ctx.fillText(text, pageWidth/2, pageHeight/2);
+
+    let text2height = ctx.measureText(text2).actualBoundingBoxDescent + ctx.measureText(text2).actualBoundingBoxAscent;
+
+    ctx.font = "9vmin League Spartan";
+    ctx.fillStyle = 'cyan';
+    let text1height = ctx.measureText(text1).actualBoundingBoxDescent + ctx.measureText(text1).actualBoundingBoxAscent;
+    ctx.fillText(text1, pageWidth/2, textTop);
+
+    ctx.fillStyle = 'white';
+    ctx.font = "6vmin League Spartan";
+    ctx.fillText(text2, pageWidth/2, textBottom + text2height/2);
+
+    textTop -= text1height;
+    textBottom += text2height;
+
+    textLeft = Math.floor((pageWidth/2) - (ctx.measureText(text2).width / 2) - radius);
+    textRight = Math.ceil((ctx.measureText(text2).actualBoundingBoxRight + (pageWidth/2) + radius));
+
+
+
+    ctx.strokeStyle = 'cyan';
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.moveTo(textLeft, textTop);
+    ctx.lineTo(textRight, textTop);
+    ctx.lineTo(textRight, textBottom);
+    ctx.lineTo(textLeft, textBottom);
+    ctx.lineTo(textLeft, textTop);
+    ctx.stroke();
+
     let collide_interbuffer = new Set();
     for (let c of firstCircles) {
 
@@ -165,7 +228,7 @@ function update() {
             let index1 = firstCircles.findIndex(i => i.id === c.id);
             let ball1 = firstCircles[index1];
             for (let collide of collision) {
-                //collision_set.add(collide.id);
+
                 if(collide.id !== c.id && !collide_buffer.has(collide.id) && !collide_buffer.has(c.id)) {
                     let index2 = firstCircles.findIndex(i => i.id === collide.id);
 
@@ -175,25 +238,41 @@ function update() {
 
                     collide_interbuffer.add(collide.id);
                     collide_interbuffer.add(c.id);
-
                 }
 
             }
         }
+        //|| (c.x >= textLeft - radius && c.x <= textRight + radius && c.y >= textTop - radius && c.y <= textBottom + radius)
 
-        if (c.x >= pageWidth - radius / 2 || c.x <= radius / 2) {
+        if(c.x >= textLeft && c.x <= textRight && c.y > textTop - radius && c.y <= pageHeight / 2) {
+            c.y = textTop - radius;
+        } else if(c.x >= textLeft && c.x <= textRight && c.y < textBottom + radius && c.y > pageHeight / 2) {
+            c.y = textBottom + radius;
+        }
+
+        if(c.y <= textBottom && c.y >= textTop && c.x > textLeft - radius && c.x <= pageWidth / 2) {
+            c.x = textLeft - radius;
+        } else if(c.y <= textBottom && c.y >= textTop && c.x < textRight + radius && c.x > pageWidth / 2) {
+            c.x = textRight + radius;
+        }
+
+
+
+        if (c.x >= pageWidth - radius / 2 || c.x <= radius / 2 || (c.y >= textTop - radius && c.y <= textBottom + radius && c.x === textLeft - radius || c.x === textRight + radius)) {
             c.forcex *= -1;
             // c.color = getRndHexColor();
         }
 
-        if (c.y >= pageHeight - radius / 2 || c.y <= radius / 2) {
+        if (c.y >= pageHeight - radius / 2 || c.y <= radius / 2 || (c.x >= textLeft - radius && c.x <= textRight + radius && c.y === textTop - radius || c.y === textBottom + radius)) {
             c.forcey *= -1;
             // c.color = getRndHexColor();
         }
 
-        c.x = c.x < radius / 2 ? radius / 2 : c.x > pageWidth - radius / 2 ? pageWidth - radius / 2 : c.x
-        c.y = c.y < radius / 2 ? radius / 2 : c.y > pageHeight - radius / 2 ? pageHeight - radius / 2 : c.y
+        c.x = c.x < radius / 2 ? radius / 2 : c.x > pageWidth - radius / 2 ? pageWidth - radius / 2 : c.x;
+        c.y = c.y < radius / 2 ? radius / 2 : c.y > pageHeight - radius / 2 ? pageHeight - radius / 2 : c.y;
         // c.radius = radius + getRndInteger(-radius / 10, radius / 10);
+
+
 
     }
     // for (let col_id of collision_set) {
@@ -209,16 +288,39 @@ function update() {
 
 
     collide_buffer = collide_interbuffer;
+    // k++;
+    // if (k > 8) {
+    //     j += pageWidth / 100;
+    //     i += pageHeight / 100;
+    //     k = 0;
+    // }
 
-    window.requestAnimationFrame(update);
+    if (tick === true) {
+        j += 0.5;
+    } else {
+        j -= 0.5;
+    }
+
+    animationFrame = window.requestAnimationFrame(update);
 
 
 }
 
-function requestUpdate() {
+function resize() {
+    window.cancelAnimationFrame(animationFrame);
+    pageHeight = window.innerHeight;
+    pageWidth = window.innerWidth;
 
+    ctx.canvas.width  = pageWidth;
+    ctx.canvas.height = pageHeight;
+    firstCircles = [];
+    vmin = pageHeight < pageWidth ? pageHeight / 100 : pageWidth / 100;
+    vmax = pageHeight >= pageWidth ? pageHeight / 100 : pageWidth / 100;
+    firstDraw();
 }
 
 
 
 window.addEventListener('load', firstDraw);
+
+window.onresize = resize;
